cfbf944b8c054cabfb201108bff863a185f62983
Ng Zhe Kai <ngzhekai@gmail.com>
2023-06-29 21:54:52 +0800

    refactor: 💡 update

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
 import { useGetWeather } from './src/hooks/useGetWeather';
 import ErrorItem from './src/components/ErrorItem';
+import Counter from './src/demonstration/Counter';
 
 const App = () => {
   const [loading, errorMsg, weather] = useGetWeather();
@@ -24,6 +25,7 @@ const App = () => {
         <ActivityIndicator size={'large'} color={'purple'} />
       )}
     </View>
+    // <Counter />
   );
 };
 
